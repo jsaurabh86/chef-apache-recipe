8adce475c25df31f267f9d3c6330cc972008bb4c
vagrant <vagrant@localhost.localdomain>
2016-03-08 22:47:50 +0000

    Initial commit

---
+++ recipes/setup.rb
@@ -0,0 +1,18 @@
+package 'cowsay' do
+	 action :install
+end
+
+package 'tree' do
+	action :install
+end
+
+package 'git' do
+	action :install
+end
+
+file '/etc/motd' do
+	content 'Property of ...'
+	mode '0644'
+end
+
+
